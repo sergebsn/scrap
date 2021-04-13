8ba9201111de778456ed517516240faeafafd754
sergebsn <sergebsn@gmail.com>
2021-04-13 23:02:34 +0300

    it's work now

--- hm1_task_2.py
+++ hm1_task_2.py
@@ -6,14 +6,21 @@ from pprint import pprint
 
 load_dotenv()
 
-
 def get_weather(city, appid):
     url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={appid}'
     return requests.get(url)
 
+path = "cred.txt"
+with open(path, "r") as f:
+    #tmp = f.readlines()
+#print(tmp)
 
-key = os.getenv('cred.txt') # я не понимаю что не так, на сайте openweathermap зарегился, создал ключ апи, сделал env,
-# положил в файл свой логин и пароль и ключ АПИ
+    USERNAME, PASSWORD, KEY = f.readlines()
+    USERNAME = USERNAME.strip()
+    PASSWORD = PASSWORD.strip()
+    print(USERNAME, PASSWORD, KEY)
+
+
 city = input('Enter the city name:')
-r = get_weather(city, key)
+r = get_weather(city, KEY)
 pprint(dict(r.json()))
