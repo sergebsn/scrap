252eefda2f75fcbc947a2056f3e09d9251a6b013
sergebsn <sergebsn@gmail.com>
2021-04-21 13:13:57 +0300

    Finished

--- hm2_task.py
+++ hm2_task.py
@@ -1,48 +1,97 @@
-import json
+# import json
 import requests
 from pprint import pprint
 from bs4 import BeautifulSoup as bs
 from fp.fp import FreeProxy
 import pandas as pd
+import time
 
-
-
-def my_vacancy(vacancy, header, proxy, area=0):
-    vacancy = vacancy.replace(' ', '+')
-    url = 'https://nn.hh.ru/search/vacancy/'
-    param = {
-        "text": f'{vacancy}',
-        "area": f'{area}',  # area 1 - Moscow, 66 - Nizghnii Novgorod
-        "order_by": 'relevance',
-        "items_on_page": '50',
-        "page": "0"
-    }
-    req = requests.get(url, headers=header, params=param, proxies=proxy)
-    return req
-
-headers = {"User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36"}
-
+url = 'https://hh.ru/search/vacancy/'
+headers = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36"}
 proxies = {
     'http': f'{FreeProxy().get()}'
+}
+
+
+# def get(url, headers, params):
+#  req = requests.get(url, headers=headers, params=params, proxies=proxies)
+# return req
+
+# params = {
+#        "clusters": "true",
+#        "enable_snippets": "true",
+#        "salary": {"st": "searchVacancy"},
+#        "text": f'{vacancy}',
+#        "showClusters": "true",
+#        "page": 0,
+#    }
+
+def get_vacancy(url, vacancy, headers, proxies, page):
+    params = {
+        "clusters": "true",
+        "text": f'{vacancy}',
+        "showClusters": "true",
+        "page": f'{page}',
     }
+    r = requests.get(url, headers=headers, params=params, proxies=proxies)
+    return bs(r.text, "html.parser")
 
 
-req = my_vacancy('повар', headers, proxies, 0)
-soup = bs(req.text, "html.parser")
+def get_vacancy_info(vacancy):
+    items_info = []
+    i = 0
+    soup = get_vacancy(url, vacancy, headers, proxies, str(i))
+    while True:
+        items = soup.find_all(attrs={"class": "vacancy-serp-item"})
+        # item_list = soup.find(attrs={"class": "vacancy-serp-item"})
+        for item in items:
+            info = {}
+            a = item.find("a", attrs={"class": "bloko-link", "data-qa": "vacancy-serp__vacancy-title"})
+            info["vacancy"] = a.getText().replace(u"\xa0", " ")
+            info["href"] = a.attrs["href"]
+            info["web"] = url
 
-item_list = soup.find(attrs={"class": "vacancy-serp"})
-items = item_list.findChildren(recursive=False)
+            employer = item.find("a", attrs={"class": "bloko-link bloko-link_secondary",
+                                             "data-qa": "vacancy-serp__vacancy-emloyer"})
 
-items_info = []
-for item in items:
-    info = {}
-    try:
-        a = item.find("a")
-        info['Link'] = a.attrs["href"]
-        info['Name'] = a.get_text()
-        #info['zp0'] = a.find(attrs={"data-qa":"vacancy-serp__vacancy-compensation"}).text
-    except Exception as e:
-        print(e)
+            if employer is None:
+                info["employer"] = ''
+            else:
+                info["employer"] = employer.getText().replace(u"\xa0", " ")
 
-df = pd.DataFrame([info])
-df.to_csv("df.csv")
+            salary = item.find("span", attrs={"data-qa": "vacancy-serp__vacancy-compensation"})
+
+            if salary is not None:
+                salary = salary.getText().replace("\xa0", " ")
+                salary = salary.replace(u"\202f", " ")
+                salary = salary.replace("-", " ")
+                salary = salary.split()
+
+                if salary[0] == 'от':
+                    info["salary_min"] = salary[1]
+                    info["salary_max"] = None
+                    info["currency"] = salary[2]
+                elif salary[0] == 'до':
+                    info["salary_min"] = None
+                    info["salary_max"] = salary[1]
+                    info["currency"] = salary[2]
+                else:
+                    info["salary_min"] = salary[0]
+                    info["salary_max"] = salary[1]
+                    info["currency"] = salary[2]
+                items_info.append(info)
+            i += 1
+            if soup.find(attrs={"class": "bloko-button", "data-qa": "pager-next"}) is None:
+                break
+            soup = get_vacancy(url, vacancy, headers, proxies, str(i))
+        return items_info
+
+
+if __name__ == '__main__':
+    vacancy = input("Введите слова для поиска:")
+    vacancy_info = get_vacancy_info(vacancy)
+    df = pd.DataFrame.from_records(vacancy_info)
+    df.to_csv("df.csv")
+    
+print()
